eee41d36acc92001c3ab8b3f0009e267ed67061e
imoncrack27 <mingodrexell321@gmail.com>
2025-04-15 08:58:15 +0800

    installed the client side frontend using antd for the UI design

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "antd";
+
+const App = () => {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Habit Tracker App</h1>
+      <Button type="primary">Test Ant Design Button</Button>
+    </div>
+  );
+};
+
+export default App;
